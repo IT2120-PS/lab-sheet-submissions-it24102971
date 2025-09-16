1381ecf7a555eaa1f63546dbabdd537639d3138f
it24102971 <it24102971@my.sliit.lk>
2025-09-16 17:05:22 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102971_Ps_Lab.R
@@ -0,0 +1,10 @@
+setwd("C://Users//Asus//Desktop//IT24102971")
+#1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+#3
+#(i)
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+#(ii)
+qnorm(0.95, mean=100, sd=15, lower.tail=TRUE)
